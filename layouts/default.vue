4b590709e202fdae6065e6217a57f0897b6405b0
jackdishman <jdish123@gmail.com>
2021-05-13 16:37:38 -0700

    added socials

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-20 sm:mb-0">
+  <div class="mb-20 sm:mb-0 emoji-cursor">
     <div class="flex flex-col items-center">
       <header
         class="md:ml-32 flex flex-col items-center fixed top-0 text-xl tracking-widest bg-white opacity-75 border-b border-l border-r border-black rounded-br rounded-bl px-2 z-50"
@@ -7,17 +7,35 @@
         <nuxt-link to="/" class="handwriting">www.jackdishman.com</nuxt-link>
       </header>
     </div>
+
     <section class="flex flex-row justify-between">
-      <article class="md:mt-10 md:mt-5 md:ml-5">
-        <div class="hidden sm:block">
-          <nuxt-link to="/work" class="handwriting">Work</nuxt-link>
-        </div>
-        <div class="hidden sm:block">
-          <nuxt-link to="/projects" class="handwriting">Projects</nuxt-link>
-        </div>
-        <div class="hidden sm:block">
-          <nuxt-link to="/education" class="handwriting">Education</nuxt-link>
-        </div>
+      <article class="md:mt-10 md:mt-5 md:ml-5 hidden sm:block handwriting">
+        <nuxt-link to="/work" class="handwriting block py-2">Work</nuxt-link>
+        <nuxt-link to="/projects" class="handwriting block py-2">Projects</nuxt-link>
+        <nuxt-link to="/education" class="handwriting block py-2"
+          >Education</nuxt-link
+        >
+        <a
+          href="https://twitter.com/jackdishman"
+          target="_blank"
+          class="handwriting block py-2"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://github.com/jackdishman"
+          target="_blank"
+          class="handwriting block py-2"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://linkedin.com/in/jackdishman"
+          target="_blank"
+          class="handwriting block py-2"
+        >
+          LinkedIn
+        </a>
       </article>
 
       <div v-if="isMobileMenuOpen" @click="toggleMobileNav()">
@@ -48,6 +66,68 @@
               Education
             </li></nuxt-link
           >
+          <li
+            class="h-16 align-middle py-2 handwriting border-t border-blue-500 pt-5"
+          >
+            <div class="flex flex-row justify-around items-center">
+              <a href="https://twitter.com/jackdishman" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-twitter"
+                >
+                  <path
+                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+                  ></path>
+                </svg>
+              </a>
+              <a href="https://github.com/jackdishman" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-github"
+                >
+                  <path
+                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                  ></path>
+                </svg>
+              </a>
+              <a href="https://linkedin.com/in/jackdishman" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-linkedin"
+                >
+                  <path
+                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                  ></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle>
+                </svg>
+              </a>
+            </div>
+          </li>
         </ul>
 
         <!-- Mobile Nav is CLOSED -->
@@ -83,6 +163,10 @@ export default {
 </script>
 
 <style>
+.emoji-cursor {
+  cursor: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>🚀</text></svg>"),
+    auto;
+}
 .handwriting {
   font-family: "Permanent Marker", cursive;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <section class="flex flex-col items-center relative">
-      <article class="absolute bottom-0 mb-10">
+    <section class="flex flex-col items-center relative bg-black h-screen">
+      <article class="fixed bottom-0 mb-64 lg:mb-20">
         <Nametag name="Jack Dishman" />
       </article>
       <article>
-        <img src="../static/images/me-1.png" class="max-h-screen" alt="Handsome picture of myself" />
+        <img src="../static/images/me.png" class="max-h-screen rounded-full" alt="Handsome picture of myself" />
       </article>
     </section>
 
